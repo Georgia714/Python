4fa51d8647255f63fee21c282617f9424510ccb8
Georgia714 <71225711+GeorgiaD714@users.noreply.github.com>
2020-11-22 15:11:01 +0000

    Update password.py

--- password.py
+++ password.py
@@ -13,7 +13,8 @@ def password_checker():
             elif password[i].islower():
                 l += 1
             elif password[i] in special_characters:
-            #((password[i] == '@') or (password[i] == "!") or (password[i] == "$") or (password[i] == "£") or (password[i] == "#") or (password[i] == "%") or (password[i] == "&") or (password[i] == "*") or (password[i] == "?") or (password[i] == "/") or (password[i] == "~")):
+            #((password[i] == '@') or (password[i] == "!") or (password[i] == "$") or (password[i] == "£") or (password[i] == "#") or (password[i] == "%") or 
+            (password[i] == "&") or (password[i] == "*") or (password[i] == "?") or (password[i] == "/") or (password[i] == "~")):
                 sc += 1
             elif password[i].isdigit():
                 n += 1
