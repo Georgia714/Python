a4e0d3dca38d80b37c40ea7c9b36fcb355f44d6c
Georgia714 <71225711+GeorgiaD714@users.noreply.github.com>
2021-03-01 11:22:13 +0000

    Update datetime.py

--- datetime.py
+++ datetime.py
@@ -1,7 +1,9 @@
+#This program calculates the total number of seconds the user has been alive.
+
 from datetime import datetime, date, time, timedelta
 td = date.today()
-bd = date(year = 1990, month = 11, day = 14)
+bd = date(year = 1991, month = 1, day = 2)
 difference = td - bd
 print("You were born", difference.days, "days ago.")
-g_seconds = difference.total_seconds()
-print("You have been alive for", int(g_seconds), "seconds.")
+t_seconds = difference.total_seconds()
+print("You have been alive for", int(t_seconds), "seconds.")
