f715d4b51fd8802e8d200683286d91ee5e5e405e
Georgia714 <71225711+Georgia714@users.noreply.github.com>
2021-04-03 11:46:10 +0100

    Update misc45.py

--- misc45.py
+++ misc45.py
@@ -1,5 +1,5 @@
 
-#SLIDE 40
+
 def favourite_meal():
     starter = input("Enter your favourite starter: ")
     main = input("Enter your favourite main course: ")
@@ -15,43 +15,9 @@ number_years = 0
 while motorbike_cost > 1000:
     motorbike_cost = motorbike_cost * 0.9
     number_years += 1
-#print("Motorbike cost after {} year(s) = £{:.2f}".format(number_years, motorbike_cost))
-
 
 print("Motorbike cost after {} years = £{:.2f}".format(number_years, motorbike_cost))
 
-
-#SLIDE 41
-
-import math
-
-def calculator():
-    number_1 = int(input("Enter a number: "))
-    number_2 = int(input("Enter a number: "))
-    operators = "+, -, *, /, **"
-    operator = input("Choose an operator from this list: {} ".format(operators))
-    if operator not in operators:
-        print("Invalid operator, choose again.", operator)
-    elif operator == "+":
-        print(number_1 + number_2)
-    elif operator == "-":
-        if number_1 > number_2:
-            print(number_1 - number_2)
-        else:
-            print(number_2 - number_1)
-    elif operator == "*":
-        print(number_1 * number_2)
-    elif operator == "/":
-        if number_1 > number_2:
-            print(math.floor(number_1 / number_2))
-        else:
-            print(math.floor(number_2 / number_1))
-    elif operator == "**":
-        print(number_1 ** number_2)
-
-calculator()
-
-#SLIDE 42
 favourite_number = int(input("Enter your favourite number: "))
 
 def joke(number):
